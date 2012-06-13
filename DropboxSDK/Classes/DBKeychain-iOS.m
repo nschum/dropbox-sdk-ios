12ca23de61b33880c93816828ee67df83b8b05ce
Nikolaj Schumacher <git@nschum.de>
2012-06-14 00:28:54 +0200

    Fixed format warnings.

--- DropboxSDK/Classes/DBKeychain-iOS.m
+++ DropboxSDK/Classes/DBKeychain-iOS.m
@@ -39,7 +39,7 @@ static NSDictionary *kDBKeychainDict;
 		return [NSKeyedUnarchiver unarchiveObjectWithData:foundValue];
 	} else {
 		if (status != errSecItemNotFound) {
-			DBLogWarning(@"DropboxSDK: error reading stored credentails (%d)", status);
+			DBLogWarning(@"DropboxSDK: error reading stored credentails (%ld)", status);
 		}
 		return nil;
 	}
@@ -66,7 +66,7 @@ static NSDictionary *kDBKeychainDict;
 	}
 
 	if (status != noErr) {
-		DBLogWarning(@"DropboxSDK: error saving credentails (%d)", status);
+		DBLogWarning(@"DropboxSDK: error saving credentails (%ld)", status);
 	}
 }
 
@@ -74,7 +74,7 @@ static NSDictionary *kDBKeychainDict;
 	OSStatus status = SecItemDelete((CFDictionaryRef)kDBKeychainDict);
 
 	if (status != noErr) {
-		DBLogWarning(@"DropboxSDK: error deleting credentials (%d)", status);
+		DBLogWarning(@"DropboxSDK: error deleting credentials (%ld)", status);
 	}
 }
 
